d5a4aaab83ebd8bc4ef15466494cceb0904b18ad
Tobias Meier <tobias.mei@edu.tbz.ch>
2024-12-11 19:08:18 +0100

    fix api

--- frontend/src/src/api.js
+++ frontend/src/src/api.js
@@ -2,13 +2,20 @@ import axios from 'axios';
 import { defineStore } from 'pinia';
 import { ref, computed } from 'vue';
 
+// Define the store
 export const useApiStore = defineStore('api', () => {
   const baseURL = ref(window.config.baseURL || '');
-  const client = computed(() => axios.create({
-    baseURL: baseURL.value, // now a new client will be created reacting to the baseURL change
-  }));
+  console.log('Initial baseURL:', baseURL.value); // Print the initial baseURL
+
+  const client = computed(() => {
+    console.log('Creating axios client with baseURL:', baseURL.value); // Print the baseURL when creating the axios client
+    return axios.create({
+      baseURL: baseURL.value, // now a new client will be created reacting to the baseURL change
+    });
+  });
 
   const setBaseURL = (url) => {
+    console.log('Setting new baseURL:', url); // Print the new baseURL when it is set
     baseURL.value = url;
   };
 
@@ -19,5 +26,6 @@ export const useApiStore = defineStore('api', () => {
 const api = axios.create({
   baseURL: window.config.baseURL || '',
 });
+console.log('Default axios instance baseURL:', api.defaults.baseURL); // Print the baseURL of the default axios instance
 
 export default api;
